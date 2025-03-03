990536136cce6a1686e9be0c5d10cfe0c188e87d
usama2333 <usama.wizz@gmail.com>
2025-03-04 01:56:37 +0500

    persist states in local storage using redux persistence

--- src/providers/ReduxProvider.js
+++ src/providers/ReduxProvider.js
@@ -1,10 +1,20 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { store } from "../redux/store";
+import { persistor, store } from "../redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
+// import { wrapper, store, persistor } from '../store/store';
+
 
 const ReduxProvider = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return(
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+        {children}
+        </PersistGate>
+    </Provider>
+
+  )
 };
 
 export default ReduxProvider;
