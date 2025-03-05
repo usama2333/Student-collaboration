d2d6f5a786773cc3a625e335a45081e3d1a73db7
usama2333 <usama.wizz@gmail.com>
2025-03-05 16:24:38 +0500

    add user form

--- src/app/dashboard/add-user/page.js
+++ src/app/dashboard/add-user/page.js
@@ -2,9 +2,25 @@
 
 import { useForm } from "react-hook-form";
 import { ToastContainer, toast } from "react-toastify";
-import "../../styles/adduser.css"
+import "../../styles/adduser.css";
+import { useState } from "react";
+import Image from "next/image";
 
 export default function AddUsers() {
+  // for handling image
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedImage(file);
+    }
+  };
+
+  const handleButtonClick = () => {
+    document.getElementById("imageInput").click();
+  };
+
   const {
     register,
     handleSubmit,
@@ -24,72 +40,270 @@ export default function AddUsers() {
       draggable: true,
       theme: "light",
     });
-
-    setTimeout(() => {
-        router.push("/login"); // Redirect to login page after toast
-      }, 1000); // Wait for the toast to finish before redirecting
   };
 
-    return (
-      <>
-       <div className="main_con">
-         <div className="info_div">
+  return (
+    <>
+      <div className="main_con">
+        <div className="info_div">
           <p className="info_text">General Info</p>
-         </div>
-               {/* form container */}
-      <div className="form_main_con">
+        </div>
+        {/* form container */}
+        <div className="form_main_con">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-flex-row">
+              {/* form first haf */}
+              <div className="flex-grow-1">
+                <div className="form-group">
+                  <label
+                    htmlFor="name"
+                    className={`form-label ${errors.name ? "error-label" : ""}`}
+                  >
+                    Name{" "}
+                    {errors.name && <span className="error-asterisk">*</span>}
+                  </label>
+                  <input
+                    id="name"
+                    type="text"
+                    placeholder="Enter your name"
+                    {...register("name", { required: "Name is required" })}
+                    className="form-input"
+                  />
+                </div>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-group">
-            <label
-              htmlFor="name"
-              className={`form-label ${errors.name ? "error-label" : ""}`}
-            >
-              Name {errors.name && <span className="error-asterisk">*</span>}
-            </label>
-            <input
-              id="name"
-              type="text"
-              {...register("name", { required: "Name is required" })}
-              className="form-input"
-            />
-          </div>
+                {/* Email Field */}
+                <div className="form-group">
+                  <label
+                    htmlFor="email"
+                    className={`form-label ${
+                      errors.email ? "error-label" : ""
+                    }`}
+                  >
+                    Email{" "}
+                    {errors.email && <span className="error-asterisk">*</span>}
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    {...register("email", {
+                      required: "Email is required",
+                      pattern: {
+                        value: /^\S+@\S+$/i,
+                        message: "Invalid email format",
+                      },
+                    })}
+                    className="form-input"
+                  />
+                </div>
 
-          {/* Email Field */}
-          <div className="form-group">
-            <label
-              htmlFor="email"
-              className={`form-label ${errors.email ? "error-label" : ""}`}
-            >
-              Email {errors.email && <span className="error-asterisk">*</span>}
-            </label>
-            <input
-              id="email"
-              type="email"
-              {...register("email", {
-                required: "Email is required",
-                pattern: {
-                  value: /^\S+@\S+$/i,
-                  message: "Invalid email format",
-                },
-              })}
-              className="form-input"
-            />
-          </div>
+                <div className="form-group">
+                  <label
+                    htmlFor="designation"
+                    className={`form-label ${
+                      errors.designation ? "error-label" : ""
+                    }`}
+                  >
+                    Designation{" "}
+                    {errors.designation && (
+                      <span className="error-asterisk">*</span>
+                    )}
+                  </label>
+                  <input
+                    id="designation"
+                    type="text"
+                    placeholder="Enter your designation"
+                    {...register("designation", {
+                      required: "Designation is required",
+                    })}
+                    className="form-input"
+                  />
+                </div>
 
+                {/* Department Select */}
+                <div className="form-group">
+                  <label
+                    htmlFor="department"
+                    className={`form-label ${
+                      errors.department ? "error-label" : ""
+                    }`}
+                  >
+                    Department{" "}
+                    {errors.department && (
+                      <span className="error-asterisk">*</span>
+                    )}
+                  </label>
+                  <select
+                    {...register("department", {
+                      required: "Please select a department",
+                    })}
+                    className="form-select"
+                  >
+                    <option value="">Select a department</option>
+                    <option value="IT">IT</option>
+                    <option value="Computer Science">Computer Science</option>
+                    <option value="Engineering">Engineering</option>
+                    <option value="Medical">Medical</option>
+                  </select>
+                </div>
 
-          {/* Submit Button */}
-          <button type="submit" className="submit-button">
-            Create
-          </button>
-      
-        </form>
+                {/* Gender Select */}
+                <div className="form-group">
+                  <label
+                    htmlFor="gender"
+                    className={`form-label ${
+                      errors.department ? "error-label" : ""
+                    }`}
+                  >
+                    Gender{" "}
+                    {errors.gender && <span className="error-asterisk">*</span>}
+                  </label>
+                  <select
+                    {...register("gender", {
+                      required: "Please select a gender",
+                    })}
+                    className="form-select"
+                  >
+                    <option value="">Select a gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Other">Other</option>
+                  </select>
+                </div>
+              </div>
+
+              {/* form second haf */}
+              <div className="flex-grow-1">
+                {/* image flex */}
+
+                <div className="upload-container">
+                  <p className="upload-text">Upload Image</p>
+
+                  {/* Image Preview */}
+                  <div className="image-box">
+                    {!selectedImage && (
+                      <Image
+                      src="/images/dummy.png"
+                      alt="Upload"
+                      width={70}
+                      height={70}
+                    />
+                    )}
+                    {selectedImage && (
+                      <div className="image-preview">
+                        <img
+                          src={URL.createObjectURL(selectedImage)}
+                          alt="Selected"
+                          className="selected-img"
+                        />
+                      </div>
+                    )}
+                  </div>
+
+                  {/* Upload Button */}
+                  <div className="button-box">
+                    <button onClick={handleButtonClick} className="upload-btn">
+                      <Image
+                        src="/images/download.png"
+                        alt="Upload"
+                        className="icon"
+                        width={30} // Set the width
+                        height={30} // Set the height
+                      />
+                      Upload
+                    </button>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      id="imageInput"
+                      style={{ display: "none" }}
+                      onChange={handleImageChange}
+                    />
+                  </div>
+
+                  {/* Cancel Button */}
+                  {selectedImage && (
+    
+                    <Image
+                    onClick={() => setSelectedImage(null)}
+                        src="/images/cancel.png"
+                         alt="Cancel"
+                      className="cancel-btn"
+                        width={30} // Set the width
+                        height={30} // Set the height
+                      />
+                  )}
+                </div>
+
+                <div className="form-group">
+                  <label
+                    htmlFor="phone"
+                    className={`form-label ${
+                      errors.phone ? "error-label" : ""
+                    }`}
+                  >
+                    Phone{" "}
+                    {errors.phone && <span className="error-asterisk">*</span>}
+                  </label>
+                  <input
+                    id="phone"
+                    type="text"
+                    placeholder="Enter your phone"
+                    {...register("phone", { required: "Phone is required" })}
+                    className="form-input"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label
+                    htmlFor="cnic"
+                    className={`form-label ${errors.cnic ? "error-label" : ""}`}
+                  >
+                    Cnic{" "}
+                    {errors.cnic && <span className="error-asterisk">*</span>}
+                  </label>
+                  <input
+                    id="cnic"
+                    type="text"
+                    placeholder="Enter your cnic"
+                    {...register("cnic", { required: "Cnic is required" })}
+                    className="form-input"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label
+                    htmlFor="address"
+                    className={`form-label ${
+                      errors.address ? "error-label" : ""
+                    }`}
+                  >
+                    Address{" "}
+                    {errors.address && (
+                      <span className="error-asterisk">*</span>
+                    )}
+                  </label>
+                  <input
+                    id="address"
+                    type="text"
+                    placeholder="Enter your address"
+                    {...register("address", {
+                      required: "address is required",
+                    })}
+                    className="form-input"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Submit Button */}
+            <button type="submit" className="submit-button">
+              Create
+            </button>
+          </form>
+        </div>
+        <ToastContainer />
       </div>
-      <ToastContainer />
-         
-       </div>
-      
-      </>
-    );
-  }
-  
+    </>
+  );
+}
