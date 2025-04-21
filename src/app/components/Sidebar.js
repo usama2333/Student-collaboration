48d86e1af73c24b43f1fafa5167e134ad153f134
usama2333 <usama.wizz@gmail.com>
2025-04-21 17:24:39 +0500

    added logout button

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -4,9 +4,23 @@ import "../styles/sidebar.css";
 import Link from "next/link";
 import Image from "next/image";
 import { logo, dashboard, user } from "../utils/images"; 
-
+import { FaSignOutAlt,FaUser, FaUserPlus,FaTachometerAlt  } from 'react-icons/fa';  
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
+import AuthContext from "@/redux/auth-contex";
+import logoutApi from "../api/logoutApi";
 
 export default function Sidebar() {
+
+  const authCtx = useContext(AuthContext);
+    const router = useRouter();
+
+    const handleLogout = () => {
+   
+      logoutApi(authCtx,router,toast,setTimeout)
+    };
+  
 
   return (
     <aside className="sidebar">
@@ -21,28 +35,29 @@ export default function Sidebar() {
         <ul>
           <li>
             <Link style={{display:'flex', alignItems:'center',gap:'10px'}} href="/dashboard" className="nav-link">
-              <Image src={dashboard} alt="Dashboard Icon" />
+            <FaTachometerAlt  size={24} />
               <p>Dashboard</p>
             </Link>
           </li>
           <li>
             <Link style={{display:'flex', alignItems:'center',gap:'10px'}} href="/dashboard/users" className="nav-link">
-              <Image src={user} alt="User Icon" />
+            <FaUser size={24} />
               <p>Users</p>
             </Link>
           </li>
           <li>
-            <Link style={{display:'flex', alignItems:'center',gap:'10px'}} href="/dashboard/add-user" className="nav-link">
-              <Image src={user} alt="User Icon" />
+            <Link style={{display:'flex', alignItems:'center',gap:'10px'}} href="/dashboard" className="nav-link">
+            <FaUserPlus  size={24} />
               <p>Add User</p>
             </Link>
           </li>
-          {/* <li>
-            <Link style={{display:'flex', alignItems:'center',gap:'10px'}} href="/dashboard/chat" className="nav-link">
-              <Image src={user} alt="User Icon" />
-              <p>Chat</p>
-            </Link>
-          </li> */}
+          <li style={{position:'absolute', bottom: '40px',width:'250px', cursor:'pointer'}} onClick={handleLogout}>
+            <a style={{display:'flex', alignItems:'center',gap:'10px'}} className="nav-link">
+            <FaSignOutAlt size={24} />
+              <p>Logout</p>
+            </a>
+          </li>  
+         
         </ul>
       </nav>
     </aside>
